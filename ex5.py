d66870071466b346425d31884c7590c88db74c5f
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-05 00:38:23 +0200

    (Verables and some Converts) Complete

---
+++ ex5.py
@@ -0,0 +1,14 @@
+# The Two Verablel you Give The Values
+verablelPound = 4
+verableInche = 41
+
+# The Two Verablel Is For Convert The Pound and Inche To Kilogram and Centimeters
+poundToKilogram = verablelPound / 2.2046
+incheToCentimeters = verableInche * 2.54 
+
+# Print The Converting Value Pount to Kilogram 
+print(f'{poundToKilogram = }')
+
+# Print The Converting Value Inche to Centimeters 
+print(f'{incheToCentimeters = }')
+
